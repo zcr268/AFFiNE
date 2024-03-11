9ed7cbf0298bc599b2a5898bc457e778b9b92962
LongYinan <lynweklm@gmail.com>
2024-03-11 18:30:41 +0800

    refactor: split web and electron entry

---
+++ tools/cli/src/webpack/runtime-config.ts
@@ -0,0 +1,168 @@
+import type { BuildFlags } from '@affine/cli/config';
+import type { BlockSuiteFeatureFlags, RuntimeConfig } from '@affine/env/global';
+
+import packageJson from '../../package.json' assert { type: 'json' };
+
+const editorFlags: BlockSuiteFeatureFlags = {
+  enable_synced_doc_block: true,
+  enable_expand_database_block: false,
+  enable_bultin_ledits: false,
+};
+
+export function getRuntimeConfig(buildFlags: BuildFlags): RuntimeConfig {
+  const buildPreset: Record<BuildFlags['channel'], RuntimeConfig> = {
+    stable: {
+      enableTestProperties: false,
+      enableBroadcastChannelProvider: true,
+      enableDebugPage: true,
+      githubUrl: 'https://github.com/toeverything/AFFiNE',
+      changelogUrl: 'https://affine.pro/what-is-new',
+      downloadUrl: 'https://affine.pro/download',
+      imageProxyUrl: '/api/worker/image-proxy',
+      linkPreviewUrl: '/api/worker/link-preview',
+      enablePreloading: true,
+      enableNewSettingModal: true,
+      enableNewSettingUnstableApi: false,
+      enableSQLiteProvider: true,
+      enableMoveDatabase: false,
+      enableNotificationCenter: true,
+      enableCloud: true,
+      enableCaptcha: true,
+      enableEnhanceShareMode: false,
+      enablePayment: true,
+      enablePageHistory: true,
+      allowLocalWorkspace: false,
+      serverUrlPrefix: 'https://app.affine.pro',
+      editorFlags,
+      appVersion: packageJson.version,
+      editorVersion: packageJson.devDependencies['@blocksuite/presets'],
+      appBuildType: 'stable',
+    },
+    get beta() {
+      return {
+        ...this.stable,
+        enablePageHistory: true,
+        serverUrlPrefix: 'https://insider.affine.pro',
+        appBuildType: 'beta' as const,
+      };
+    },
+    get internal() {
+      return {
+        ...this.stable,
+        serverUrlPrefix: 'https://insider.affine.pro',
+        appBuildType: 'internal' as const,
+      };
+    },
+    // canary will be aggressive and enable all features
+    canary: {
+      enableTestProperties: true,
+      enableBroadcastChannelProvider: true,
+      enableDebugPage: true,
+      githubUrl: 'https://github.com/toeverything/AFFiNE',
+      changelogUrl: 'https://github.com/toeverything/AFFiNE/releases',
+      downloadUrl: 'https://affine.pro/download',
+      imageProxyUrl: '/api/worker/image-proxy',
+      linkPreviewUrl: '/api/worker/link-preview',
+      enablePreloading: true,
+      enableNewSettingModal: true,
+      enableNewSettingUnstableApi: false,
+      enableSQLiteProvider: true,
+      enableMoveDatabase: false,
+      enableNotificationCenter: true,
+      enableCloud: true,
+      enableCaptcha: true,
+      enableEnhanceShareMode: false,
+      enablePayment: true,
+      enablePageHistory: true,
+      allowLocalWorkspace: false,
+      serverUrlPrefix: 'https://affine.fail',
+      editorFlags,
+      appVersion: packageJson.version,
+      editorVersion: packageJson.devDependencies['@blocksuite/presets'],
+      appBuildType: 'canary',
+    },
+  };
+
+  const currentBuild = buildFlags.channel;
+
+  if (!(currentBuild in buildPreset)) {
+    throw new Error(`BUILD_TYPE ${currentBuild} is not supported`);
+  }
+
+  const currentBuildPreset = buildPreset[currentBuild];
+
+  const environmentPreset = {
+    enableTestProperties: process.env.ENABLE_TEST_PROPERTIES
+      ? process.env.ENABLE_TEST_PROPERTIES === 'true'
+      : currentBuildPreset.enableTestProperties,
+    enableBroadcastChannelProvider: process.env.ENABLE_BC_PROVIDER
+      ? process.env.ENABLE_BC_PROVIDER !== 'false'
+      : currentBuildPreset.enableBroadcastChannelProvider,
+    changelogUrl: process.env.CHANGELOG_URL ?? currentBuildPreset.changelogUrl,
+    enablePreloading: process.env.ENABLE_PRELOADING
+      ? process.env.ENABLE_PRELOADING === 'true'
+      : currentBuildPreset.enablePreloading,
+    enableNewSettingModal: process.env.ENABLE_NEW_SETTING_MODAL
+      ? process.env.ENABLE_NEW_SETTING_MODAL === 'true'
+      : currentBuildPreset.enableNewSettingModal,
+    enableSQLiteProvider: process.env.ENABLE_SQLITE_PROVIDER
+      ? process.env.ENABLE_SQLITE_PROVIDER === 'true'
+      : currentBuildPreset.enableSQLiteProvider,
+    enableNewSettingUnstableApi: process.env.ENABLE_NEW_SETTING_UNSTABLE_API
+      ? process.env.ENABLE_NEW_SETTING_UNSTABLE_API === 'true'
+      : currentBuildPreset.enableNewSettingUnstableApi,
+    enableNotificationCenter: process.env.ENABLE_NOTIFICATION_CENTER
+      ? process.env.ENABLE_NOTIFICATION_CENTER === 'true'
+      : currentBuildPreset.enableNotificationCenter,
+    enableCloud: process.env.ENABLE_CLOUD
+      ? process.env.ENABLE_CLOUD === 'true'
+      : currentBuildPreset.enableCloud,
+    enableCaptcha: process.env.ENABLE_CAPTCHA
+      ? process.env.ENABLE_CAPTCHA === 'true'
+      : buildFlags.mode === 'development'
+        ? false
+        : currentBuildPreset.enableCaptcha,
+    enableEnhanceShareMode: process.env.ENABLE_ENHANCE_SHARE_MODE
+      ? process.env.ENABLE_ENHANCE_SHARE_MODE === 'true'
+      : currentBuildPreset.enableEnhanceShareMode,
+    enableMoveDatabase: process.env.ENABLE_MOVE_DATABASE
+      ? process.env.ENABLE_MOVE_DATABASE === 'true'
+      : currentBuildPreset.enableMoveDatabase,
+    enablePayment: process.env.ENABLE_PAYMENT
+      ? process.env.ENABLE_PAYMENT !== 'false'
+      : buildFlags.mode === 'development'
+        ? true
+        : currentBuildPreset.enablePayment,
+    enablePageHistory: process.env.ENABLE_PAGE_HISTORY
+      ? process.env.ENABLE_PAGE_HISTORY === 'true'
+      : buildFlags.mode === 'development'
+        ? true
+        : currentBuildPreset.enablePageHistory,
+    allowLocalWorkspace: process.env.ALLOW_LOCAL_WORKSPACE
+      ? process.env.ALLOW_LOCAL_WORKSPACE === 'true'
+      : buildFlags.mode === 'development'
+        ? true
+        : currentBuildPreset.allowLocalWorkspace,
+    isSelfHosted: process.env.SELF_HOSTED === 'true',
+  };
+
+  const testEnvironmentPreset = {
+    allowLocalWorkspace: true,
+  };
+
+  if (buildFlags.mode === 'development') {
+    currentBuildPreset.serverUrlPrefix = 'http://localhost:8080';
+  }
+
+  return {
+    ...currentBuildPreset,
+    // environment preset will overwrite current build preset
+    // this environment variable is for debug proposes only
+    // do not put them into CI
+    ...(process.env.CI ? {} : environmentPreset),
+
+    // test environment preset will overwrite current build preset
+    // this environment variable is for github workflow e2e-test only
+    ...(process.env.IN_CI_TEST ? testEnvironmentPreset : {}),
+  };
+}
---
+++ tools/cli/src/webpack/webpack.config.ts
@@ -0,0 +1,52 @@
+import { execSync } from 'node:child_process';
+import { join, resolve } from 'node:path';
+
+import type { BuildFlags } from '@affine/cli/config';
+import HTMLPlugin from 'html-webpack-plugin';
+import { once } from 'lodash-es';
+import { merge } from 'webpack-merge';
+
+import { createConfiguration, rootPath } from './config.js';
+import { getRuntimeConfig } from './runtime-config.js';
+
+const DESCRIPTION = `There can be more than Notion and Miro. AFFiNE is a next-gen knowledge base that brings planning, sorting and creating all together.`;
+
+const gitShortHash = once(() => {
+  const { GITHUB_SHA } = process.env;
+  if (GITHUB_SHA) {
+    return GITHUB_SHA.substring(0, 9);
+  }
+  const sha = execSync(`git rev-parse --short HEAD`, {
+    encoding: 'utf-8',
+  }).trim();
+  return sha;
+});
+
+export default async function (cli_env: any, _: any) {
+  const flags: BuildFlags = JSON.parse(
+    Buffer.from(cli_env.flags, 'hex').toString('utf-8')
+  );
+  console.log('build flags', flags);
+  const runtimeConfig = getRuntimeConfig(flags);
+  console.log('runtime config', runtimeConfig);
+  const config = createConfiguration(flags, runtimeConfig);
+  return merge(config, {
+    entry: {
+      app: resolve(rootPath, 'src/index.tsx'),
+    },
+    plugins: [
+      new HTMLPlugin({
+        template: join(rootPath, '.webpack', 'template.html'),
+        inject: 'body',
+        scriptLoading: 'module',
+        minify: false,
+        chunks: ['app'],
+        filename: 'index.html',
+        templateParameters: {
+          GIT_SHORT_SHA: gitShortHash(),
+          DESCRIPTION,
+        },
+      }),
+    ],
+  });
+}
